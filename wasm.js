dfc49115817c20f7b13ac440da05f601e5219cb2
lancenonce <lancendavis47@gmail.com>
2023-03-13 10:10:10 -0400

    started fun

---
+++ wasm.js
@@ -0,0 +1,11 @@
+const { readFileSync, writeFileSync } = require("fs");
+const wabt = require("wabt");
+const path = require("path");
+
+const inputWat = "text.wat";
+const outputWasm = "text.wasm";
+
+const wasmModule = wabt.parseWat(inputWat, readFileSync(inputWat, "utf8"));
+const { buffer } = wasmModule.toBinary({});
+
+writeFileSync(outputWasm, new Buffer(buffer));
